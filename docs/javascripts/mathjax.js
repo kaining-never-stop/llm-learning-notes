4500a2607fbb785c5ea3bc31d6f66a26b03650c4
kylening <kylening@tencent.com>
2026-07-14 11:57:03 +0800

    Fix Markdown and responsive rendering

--- docs/javascripts/mathjax.js
+++ docs/javascripts/mathjax.js
@@ -1,17 +1,22 @@
 window.MathJax = {
   tex: {
-    inlineMath: [["\\(", "\\)"], ["$", "$"]],
-    displayMath: [["\\[", "\\]"], ["$$", "$$"]],
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
     processEscapes: true,
     processEnvironments: true
   },
   options: {
     ignoreHtmlClass: ".*|",
     processHtmlClass: "arithmatex"
+  },
+  startup: {
+    typeset: false
   }
 };
 
 document$.subscribe(() => {
+  MathJax.startup.output.clearCache();
+  MathJax.typesetClear();
+  MathJax.texReset();
   MathJax.typesetPromise();
 });
-
